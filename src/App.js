7b474c327c6af331af358f91841d4596f7fc81b3
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-06-24 02:13:09 -0500

    rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import AppContainer from "./containers/AppContainer";
-
-// Components
-import About from "./pages/About";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <div>
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
+      </div>
       <Routes>
-        <Route path="/" element={<p>Landing page</p>} />
-        <Route path="login" element={<p>Login</p>} />
-        <Route path="signup" element={<p>Signup</p>} />
-        <Route path="app" element={<AppContainer />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
