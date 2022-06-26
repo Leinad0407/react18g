47531f111bc4409e3d2a8c00d530b02ab2d078fe
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-06-25 22:47:12 -0500

    Rutas específicas con algún error

--- src/App.js
+++ src/App.js
@@ -9,16 +9,23 @@ function App() {
     <div className="App">
       <h1>Welcome to React Router!</h1>
       <div>
-        <nav className="navbar">
+        {/* <nav className="navbar">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/profile">Profile</Link>
-        </nav>
+        </nav> */}
       </div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<p>Landing page</p>} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/login" element={<p>Login</p>} />
+        <Route path="/signup" element={<p>Signup</p>} />
+        <Route path="app" element={<AppContainer />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </div>
   );
