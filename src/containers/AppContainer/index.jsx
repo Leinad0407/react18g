c14035b15d6935c56e922ea2d59ed80e2a09f125
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-06-26 15:56:17 -0500

    Navegacion entre paginas

--- src/containers/AppContainer/index.jsx
+++ src/containers/AppContainer/index.jsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function AppContainer() {
   return (
     <div>
       <h1>App</h1>
+      <nav className="navbar">
+        <Link to="">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="profile">Profile</Link>
+      </nav>
       <Outlet />
     </div>
   );
